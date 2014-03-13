2e1d30d47224c887893149dcc901cb00d7f3db46
scottlow <slow@uvic.ca>
2014-03-12 20:59:22 -0700

    Initial commit.

---
+++ server/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
---
+++ server/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from server.serializers import UserSerializer, GroupSerializer
+from rest_framework import permissions
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)    
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
