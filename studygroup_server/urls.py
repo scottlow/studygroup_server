14bc5452a767f21f6a00767e8e8643b151c806fb
Paul Moon <moon.haein@gmail.com>
2014-03-30 22:11:50 -0700

    Added a Create Session REST endpoint.
    Also included a sample CURL command for creating a new session in
    sample_commands.

--- studygroup_server/urls.py
+++ studygroup_server/urls.py
@@ -19,5 +19,6 @@ urlpatterns = patterns('',
     url(r'^locations/university/(?P<universityID>.+)/?$', views.UniversityLocationsView.as_view()),     
     url(r'^users/profile/?', views.StudentProfileView.as_view()),
     url(r'^sessions/courses/(id=\d+&)*(id=\d+)/?$', views.SessionPerCourseView.as_view()),    
-    url(r'^sessions/university/(?P<universityID>.+)/?$', views.SessionByUniversityView.as_view()),    
+    url(r'^sessions/create/?$', views.SessionCreateView.as_view()),
+    url(r'^sessions/university/(?P<universityID>.+)/?$', views.SessionByUniversityView.as_view()),
 )
