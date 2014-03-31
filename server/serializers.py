7b9dbe07a8b171390a4c85b7e47d9235c0e78967
jguze <jaguze@gmail.com>
2014-03-31 02:20:59 -0700

    Added the name to the coordinator in our getSessions

--- server/serializers.py
+++ server/serializers.py
@@ -7,6 +7,11 @@ class CourseSerializer(serializers.ModelSerializer):
         model = server.models.Course
         fields = ('id', 'name', 'start_date', 'end_date')
 
+class MinimalCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = server.models.Course
+        fields = ('id', 'name')
+
 class UniversitySerializer(serializers.ModelSerializer):
     class Meta:
         model = server.models.University
@@ -19,12 +24,19 @@ class StudentSerializer(serializers.ModelSerializer):
         model = server.models.Student
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'courses', 'university')        
 
+class MinimalStudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = server.models.Student
+        fields = ('id', 'username')
+
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = server.models.Location
         fields = ('id', 'latitude', 'longitude', 'name', 'university' ,'room_number', 'frequency')
 
 class SessionSerializer(serializers.ModelSerializer):
+    course = MinimalCourseSerializer(many=False)
+    coordinator = MinimalStudentSerializer(many=False)
     class Meta:
         model = server.models.Session
         fields = ('id', 'coordinator', 'latitude', 'longitude', 'course', 'location', 'attendees', 'start_time', 'end_time')
